f3bfcfd0812698475f8723d8cafa15e51c776ee4
Jobet Guarte <jobetguartejg@gmail.com>
2024-03-09 13:27:39 +0800

    Update database

--- utils/database.js
+++ utils/database.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 
 let isConnected = false; // track the connection
+const mongoURI = process.env.MONGODB_URI;
 
 export const connectToDB = async () => {
   mongoose.set('strictQuery', true);
@@ -11,7 +12,7 @@ export const connectToDB = async () => {
   }
 
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
+    await mongoose.connect(mongoURI, {
       dbName: "share_prompt",
       useNewUrlParser: true,
       useUnifiedTopology: true,
