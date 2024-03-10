e6d611388075b6d1dfaa8ecb7ed2d394e9203859
Jobet Guarte <jobetguartejg@gmail.com>
2024-03-10 12:26:30 +0800

    add update prompt

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
+"use client";
+
+import { useEffect, useState } from "react";
+import { useSearchParams, useRouter } from 'next/navigation'
+
+import Form from "@components/Form";
+
 
 const UpdatePrompt = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams()
+  const promptID = searchParams.get('id')
+
+  const [post, setPost] = useState({ prompt: "", tag: "", });
+  const [submitting, setIsSubmitting] = useState(false);
+
+  useEffect(() => {
+    const getPromptDetails = async () => {
+      const response = await fetch(`/api/prompt/${promptID}`);
+      const data = await response.json();
+
+      setPost({
+        prompt: data.prompt,
+        tag: data.tag,
+      });
+    };
+
+    if (promptID) getPromptDetails();
+  }, [promptID]);
+
+  const _updatePrompt = async (e) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+
+    if (!promptID) return alert("Missing PromptId!");
+
+    try {
+      const response = await fetch(`/api/prompt/${promptID}`, {
+        method: "PATCH",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          tag: post.tag,
+        }),
+      });
+
+      if (response.ok) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
   return (
-    <div>UpdatePrompt</div>
-  )
+    <Form
+      type='Edit'
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={_updatePrompt}
+    />
+  );
 }
 
 export default UpdatePrompt
