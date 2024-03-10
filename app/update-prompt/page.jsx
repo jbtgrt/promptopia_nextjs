06815b754a2395d4e309bf454d41c0e4dca98a30
Jobet Guarte <jobetguartejg@gmail.com>
2024-03-10 12:54:37 +0800

    disable suspense

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import { useSearchParams, useRouter } from 'next/navigation'
+import { useEffect, useState, Suspense  } from "react";
 
 import Form from "@components/Form";
 
